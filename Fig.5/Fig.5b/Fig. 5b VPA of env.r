7c3c4642cac8e673788e83854bfb898b354e9395
MengzhiJ <84562318+MengzhiJ@users.noreply.github.com>
2024-01-28 00:41:48 +0800

    Update Fig. 5b VPA of env.r

--- Fig.5/Fig.5b/Fig. 5b VPA of env.r
+++ Fig.5/Fig.5b/Fig. 5b VPA of env.r
@@ -9,7 +9,7 @@ env<-read.csv('H:/Tidal/R_data/Fig.4/Environment.csv',row.names = 1) #screen env
 print(row.names(votu_matrix)==row.names(env))
 res_all<- cca(votu_matrix~.,data=env)
 res_null<-cca(votu_matrix~1,data=env)
-env_frwd<-ordistep(res_null,scope=formula(res_all),direction = 'forward',permutations = how(nperm = 199)) #fit model<0.05
+env_frwd<-ordistep(res_null,scope=formula(res_all),direction = 'forward',permutations = how(nperm = 499)) #fit model<0.05
 env_frwd #no-linear envs
 
 geo<-read.csv('H:/Tidal/R_data/Fig.4/Distance.csv',row.names = 1)  #calculate PCNM for geo
@@ -18,7 +18,7 @@ geo<-pcnm(geo)
 geo<-as.data.frame(geo$vectors)
 res_all<- cca(votu_matrix~.,data=geo)
 res_null<-cca(votu_matrix~1,data=geo)
-env_frwd<-ordistep(res_null,scope=formula(res_all),direction = 'forward',permutations = how(nperm = 999)) #fit model<0.05
+env_frwd<-ordistep(res_null,scope=formula(res_all),direction = 'forward',permutations = how(nperm = 499)) #fit model<0.05
 env_frwd #no-linear envs
 
 write.csv(geo,file='H:/Tidal/R_data/Fig.4/PCNM.csv',row.names = T)
@@ -30,4 +30,3 @@ env<-envdat_raw[,1:10]
 geo<-envdat_raw[,12:18]
 cca_all<-varpart(votu_matrix,env,geo,chisquare = T) #obtain one or two factors common proportion
 cca_all
-decorana(votu_matrix)
