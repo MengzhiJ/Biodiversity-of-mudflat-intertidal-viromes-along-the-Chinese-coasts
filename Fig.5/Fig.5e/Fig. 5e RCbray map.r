90cfbcfdf6c864e7def5ebef4c08b03703f6d56f
MengzhiJ <84562318+MengzhiJ@users.noreply.github.com>
2024-03-21 22:10:57 +0800

    Update Fig. 5e RCbray map.r

--- Fig.5/Fig.5e/Fig. 5e RCbray map.r
+++ Fig.5/Fig.5e/Fig. 5e RCbray map.r
@@ -2,7 +2,8 @@ library(ggplot2)
 library(reshape2)
 library(dplyr)
 library(readxl)
-data<-read_excel("I:/Tidal/R_data/Fig.5/RCbray.xlsx")
+setwd("Fig.5")
+data<-read_excel("RCbray.xlsx")
 data$class <- factor(data$class,level=c("vOTUs","mOTUs","Virus (host)","Host"))
 ggplot(data,aes(x=class,y=Ratio,fill=level))+
   geom_bar(stat="identity", color="NA",width=0.7,size=0.4,position="stack", aes(fill=level),alpha=0.8)+
@@ -15,4 +16,4 @@ ggplot(data,aes(x=class,y=Ratio,fill=level))+
                      panel.border = element_rect(size=1),
                      legend.text = element_text(size=15),
                      legend.title = element_text(size=16))+theme(legend.position = 'none')
-ggsave("Fig.5d.tiff",width=5.9,height=5.6, path="I:/Tidal/R_data/Fig.5/")
+
